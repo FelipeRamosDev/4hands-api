774ba9421d3f3984a507a388ce27d6a8c9b229f8
Felipe Ramos <86696152+FelipeRamos1202@users.noreply.github.com>
2023-09-21 13:01:01 -0300

    [HAPI-7] Implement dynamic configs for ServerAPI (#4)

--- src/services/ServerAPI/index.js
+++ src/services/ServerAPI/index.js
@@ -7,39 +7,62 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const https = require('https');
 
 // Routes
 const routes = require('@routes/index');
 
 module.exports = class ServerAPI {
     constructor (setup) {
-        const { API_SECRET, sessionMaxAge, staticPath, PORT, listenCallback } = Object(setup);
-        const rootPath = __dirname.replace('\\node_modules\\4hands-api\\src\\services', '\\').replace(/\\/g, '/');
+        const {
+            API_SECRET,
+            sessionCookiesMaxAge,
+            staticPath,
+            listenCallback,
+            compileFE,
+            jsonLimit,
+            sessionResave,
+            sessionSaveUninitialized,
+            keySSLPath,
+            certSSLPath
+        } = Object(setup);
 
+        let { PORT } = Object(setup);
+        let useSSL = false;
+        
+        if (!PORT) {
+            PORT = 80;
+        }
+        
+        this.rootPath = __dirname.replace('\\node_modules\\4hands-api\\src\\services', '\\').replace(/\\/g, '/');
         this.app = express();
         this.serverState = 'loading';
 
-        // // Compiling frontend code
-        // const compile = execSync('npm run build');
-        // // Printiting webpack compile result
-        // console.log(compile.toString());
+        if (compileFE) {
+            // Compiling frontend code
+            const compile = execSync('npm run build');
+            // Printiting webpack compile result
+            console.log(compile.toString());
+        }
 
         // Configuring server
         this.app.use(cors());
-        this.app.use(bodyParser.json({ limit: '10mb' }));
+        this.app.use(bodyParser.json({ limit: jsonLimit || '10mb' }));
         this.app.use(express.json());
 
         if (staticPath) {
-            this.app.use(express.static(rootPath + staticPath));
+            this.app.use(express.static(this.rootPath + staticPath));
         }
 
-        if (API_SECRET && sessionMaxAge) {
+        if (API_SECRET) {
             this.app.use(session({
                 secret: API_SECRET,
-                resave: true,
-                saveUninitialized: true,
-                cookie: { maxAge: sessionMaxAge }
+                resave: (sessionResave !== undefined) ? sessionResave : true,
+                saveUninitialized: (sessionSaveUninitialized !== undefined) ? sessionSaveUninitialized : true,
+                cookie: { maxAge: sessionCookiesMaxAge || 86400000 }
             }));
+        } else {
+            throw 'You need to provide a API SECRET to start the server!';
         }
 
         // Standard routes
@@ -47,13 +70,68 @@ module.exports = class ServerAPI {
         this.app.use('/auth', routes.auth);
         this.app.use('/collection', routes.collection);
 
-        if (PORT) {
-            this.app.listen(PORT, listenCallback);
-            this.serverState = 'online';
+        if (keySSLPath && certSSLPath) {
+            useSSL = true;
+        }
+
+        if (useSSL) {
+            const SSL_KEY = fs.readFileSync(this.rootPath + keySSLPath);
+            const SSL_CERT = fs.readFileSync(this.rootPath + certSSLPath);
+
+            if (SSL_KEY && SSL_CERT) {
+                const options = {
+                    key: SSL_KEY,
+                    cert: SSL_CERT
+                };
+
+                https.createServer(options, app).listen(PORT, () => {
+                    console.log(`Server listening on port ${PORT}`);
+                    this.PORT = PORT;
+                });
+            } else {
+                throw new Error.Log({
+                    name: 'SSLCertificateNotFound',
+                    message: `The SSL certificate wasn't found on the directory!`
+                });
+            }
+        } else {
+            this.app.listen(PORT, () => {
+                this.PORT = PORT;
+                typeof listenCallback === 'function' && listenCallback();
+            });
         }
     }
 
-    listen(port, cb) {
-        this.app.listen(port, cb);
+    listen(PORT, callback) {
+        if (!this.PORT && PORT) {
+            this.app.listen(PORT, () => {
+                this.PORT = PORT;
+                typeof callback === 'function' && callback();
+            });
+        }
+    }
+
+    listenSSL(PORT, keySSLPath, certSSLPath, callback) {
+        if (!this.PORT && PORT) {
+            const SSL_KEY = fs.readFileSync(this.rootPath + keySSLPath);
+            const SSL_CERT = fs.readFileSync(this.rootPath + certSSLPath);
+
+            if (!SSL_KEY || !SSL_CERT) {
+                throw new Error.Log({
+                    name: 'SSLCertificateNotFound',
+                    message: `The SSL certificate wasn't found on the directory!`
+                });
+            }
+
+            const options = {
+                key: SSL_KEY,
+                cert: SSL_CERT
+            };
+
+            https.createServer(options, this.app).listen(PORT, () => {
+                this.PORT = PORT;
+                typeof callback === 'function' && callback();
+            });
+        }
     }
 }
