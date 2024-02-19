ad902fb2af762a52587e5a3011de31537e6ded8d
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-02-18 22:45:09 -0300

    [HAPI-88] Complete a CRUD methods (#100)
    ## [HAPI-88] Complete a CRUD methods

--- src/services/Redis/index.js
+++ src/services/Redis/index.js
@@ -1,5 +1,6 @@
 const { createClient } = require('redis');
 const crypto = require('crypto');
+const { buildKey } = require('./RedisHelpers');
 
 class RedisService {
     constructor(setup) {
@@ -21,7 +22,8 @@ class RedisService {
 
     async connect() {
         try {
-            return await this.client.connect();
+            this.connection = await this.client.connect();
+            return this;
         } catch (err) {
             throw new Error.Log(err);
         }
@@ -29,6 +31,7 @@ class RedisService {
 
     async disconnect() {
         try {
+            delete this.connection;
             return await this.client.disconnect();
         } catch (err) {
             throw new Error.Log(err);
@@ -43,15 +46,37 @@ class RedisService {
 
     async createDoc(setup) {
         let { collection, uid, data } = Object(setup);
-        const setters = [];
 
         if (!uid) {
             uid = crypto.randomBytes(10).toString('hex');
         }
 
         try {
+            return await this.setDoc({ collection, uid, data });
+        } catch (err) {
+            throw new Error.Log(err);
+        }
+    }
+
+    async setDoc(setup) {
+        const { collection, uid, data } = Object(setup);
+        const setters = [];
+
+        try {
+            if (!collection || typeof collection !== 'string') {
+                throw new Error.Log('commom.bad_format_param', 'collection', 'RedisService.setDoc', 'string', collection);
+            }
+
+            if (!uid || typeof uid !== 'string') {
+                throw new Error.Log('commom.bad_format_param', 'uid', 'RedisService.setDoc', 'string', uid);
+            }
+
+            if (!data || typeof data !== 'object' || Array.isArray(data)) {
+                return;
+            }
+
             Object.keys(data).map(key => {
-                setters.push(this.setDocField(`${collection}:${uid}`, key, data[key]));
+                setters.push(this.setDocField({collection, uid, field: key, value: data[key]}));
             });
 
             const saved = await Promise.all(setters);
@@ -66,17 +91,50 @@ class RedisService {
     
     async getDoc({ collection, uid }) {
         try {
-            const doc = await this.client.hGetAll(`${collection}:${uid}`);
+            const doc = await this.client.hGetAll(buildKey(collection, uid));
             return doc;
         } catch (err) {
             throw new Error.Log(err);
         }
     }
 
-    async setDocField(uid, field, value) {
+    async setDocField(setup) {
+        const { collection, uid, field, value } = Object(setup);
+
         try {
-            const ready = await this.client.hSet(uid, field, value);
+            if (!collection || typeof collection !== 'string') {
+                throw new Error.Log('commom.bad_format_param', 'collection', 'RedisService.setDocField', 'string', collection);
+            }
+
+            if (!uid || typeof uid !== 'string') {
+                throw new Error.Log('commom.bad_format_param', 'uid', 'RedisService.setDocField', 'string', uid);
+            }
+
+            if (!field || typeof field !== 'string') {
+                throw new Error.Log('commom.bad_format_param', 'field', 'RedisService.setDocField', 'string', uid);
+            }
+
+            const ready = await this.client.hSet(buildKey(collection, uid), field, value);
             return { success: true, status: ready };
+        } catch (err) {
+            throw new Error.Log(err);
+        }
+    }
+
+    async deleteDoc(setup) {
+        const { collection, uid } = Object(setup);
+        const key = buildKey(collection, uid);
+        const promises = [];
+
+        try {
+            const keys = await this.client.hKeys(key);
+
+            keys.map(curr => promises.push(this.client.hDel(key, curr)));
+
+            const resolve = await Promise.all(promises);
+            if (resolve.every(success => success)) {
+                return { success: true };
+            }
         } catch (err) {
             throw new Error.Log(err);
         }
