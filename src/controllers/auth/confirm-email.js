79e478fc9721eae3e301387f5be1ffaf79f14b6f
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-03-15 19:27:44 -0300

    [HAPI-102] Fix sign-in and login errors (#119)
    ## [HAPI-102] Fix sign-in and login errors

--- src/controllers/auth/confirm-email.js
+++ src/controllers/auth/confirm-email.js
@@ -13,11 +13,21 @@ module.exports = new Endpoint({
     bodySchema: { confirmationtoken: { type: String, required: true } },
     controller: async (req, res) => {
         try {
+            const body = req.body;
+            const data = req.session;
             const user = await User.getUser(req.session?.user?._id);
-            const updated = await user.updateDB({ data: { isEmailConfirmed: true }});
+            
+            if (body.confirmationtoken === data.confirmationToken) {
+                const updated = await user.updateDB({ data: { isEmailConfirmed: true }});
+    
+                if (updated instanceof Error.Log) {
+                    throw updated;
+                }
 
-            if (updated instanceof Error.Log) {
-                throw updated;
+                data.isEmailConfirmed = true;
+            } else {
+                data.isEmailConfirmed = false;
+                return res.status(401).send(badConfirmationToken);
             }
 
             req.sessionStore.destroy(req.sessionID);
