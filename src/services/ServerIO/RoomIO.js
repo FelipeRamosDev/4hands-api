9c4a616ddf024f572a362b3021381e8305c0e01f
FelipeRamosDev <felipe@feliperamos.info>
2024-05-19 19:06:53 -0300

    [HAPI-139] Create the RoomIO instance
    ## [HAPI-139] Description
    Created the RoomIO class and the room's features

---
+++ src/services/ServerIO/RoomIO.js
@@ -0,0 +1,43 @@
+class RoomIO {
+    constructor(setup, serverIO) {
+        const {
+            id,
+            isPrivate = false,
+            participants = []
+        } = Object(setup);
+
+        this._serverIO = () => serverIO;
+        this.id = id;
+        this.isPrivate = isPrivate;
+        this.participants = participants;
+    }
+
+    get serverIO() {
+        return this._serverIO();
+    }
+
+    join(participantID) {
+        const socket = this.serverIO.getConnection(participantID);
+        
+        if (socket) {
+            socket.join(this.id);
+
+            // Push to the participants array if it's not there yet
+            if (!this.participants.find(item => item === socket.id)) {
+                this.participants.push(socket.id);
+            }
+        }
+
+        return this;
+    }
+
+    leave(participantID) {
+        const participantIndex = this.participants.indexOf(participantID);
+
+        if (participantIndex > -1) {
+            this.participants = this.participants.splice(participantIndex, participantID + 1);
+        }
+    }
+}
+
+module.exports = RoomIO;
