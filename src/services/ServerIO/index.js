804be74b2b05907566a9d0903bcfd8ec15ab4dd0
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-05-19 19:07:53 -0300

    [HAPI-139] Create the RoomIO instance (#161)
    ## [HAPI-139] Description
    Created the RoomIO class and the room's features

--- src/services/ServerIO/index.js
+++ src/services/ServerIO/index.js
@@ -1,3 +1,4 @@
+const RoomIO = require('./RoomIO');
 const socketIO = require('socket.io');
 
 class ServerIO {
@@ -8,10 +9,10 @@ class ServerIO {
      * @param {string} setup.path - Path for the new namespace that will be created.
      * @param {number} setup.port - Path for the new namespace that will be created.
      * @param {function[]} setup.middlewares - Array with middleware functions that receives (socket, next) as paramenter.
-     * @param {function} setup.onConnect - Callback.
-     * @param {function} setup.onDisconnect - Callback.
-     * @param {function} setup.onData - Callback.
-     * @param {function} setup.onError - Callback.
+     * @param {function} setup.onConnect - Callback for when the socket connection is completed with success.
+     * @param {function} setup.onDisconnect - Callback for when the client socket is disconnected.
+     * @param {function} setup.onData - Callback for the 'message' socket event.
+     * @param {function} setup.onError - Callback for errors during socket connection.
      * @returns {object} With the configs to import on ServerIO to use.
      */
     constructor(setup, serverIO) {
@@ -32,6 +33,7 @@ class ServerIO {
         this.path = path;
         this.middlewares = middlewares;
         this.namespaces = {};
+        this.rooms = {};
 
         if (!serverIO) {
             // If doesn't have the serverIO param declared, means that it's the main instance so the server should be declared
@@ -56,8 +58,12 @@ class ServerIO {
         this.io.on('connect_error', onError.bind(this));
         this.io.on('connect', (socket) => {
             socket.on('message', onData.bind(this));
-            socket.on('disconnect', () => onDisconnect.call(this, socket.id));
+            socket.on('disconnect', () => {
+                this.deleteRoom(socket.id);
+                onDisconnect.call(this, socket.id);
+            });
 
+            this.createRoom(socket.id, { participants: [socket.id], isPrivate: true});
             onConnect.call(this, socket);
         });
     }
@@ -69,9 +75,25 @@ class ServerIO {
     get mainIO() {
         return this.serverIO.io;
     }
+
+    getConnection(socketID) {
+        if (this.serverIO) {
+            return this.io.sockets.get(socketID);
+        } else {
+            return this.io.sockets.sockets.get(socketID);
+        }
+    }
     
     postMessage(data, cb) {
         this.io.emit('message', data, cb);
+    }
+
+    toRoom(roomID) {
+        return this.io.to(roomID);
+    }
+
+    postMessageTo(roomID, data, cb) {
+        this.toRoom(roomID).emit('message', data, cb);
     }
 
     broadcastMessage(data, cb) {
@@ -89,7 +111,49 @@ class ServerIO {
     }
 
     appendNamespace(namespace) {
+        if (!namespace?.path || !(namespace instanceof ServerIO)) {
+            return;
+        }
+
         this.namespaces[namespace.path] = namespace;
+    }
+
+    createRoom(id, options) {
+        const { participants, isPrivate } = Object(options);
+
+        if (typeof id !== 'string') {
+            return;
+        }
+
+        const newRoom = new RoomIO({ id, participants, isPrivate }, this);
+        this.appendRoom(newRoom);
+        return newRoom;
+    }
+
+    joinRoom(roomID, participantID) {
+        const room = this.getRoom(roomID);
+
+        if (room) {
+            room.join(participantID);
+        }
+
+        return room;
+    }
+
+    deleteRoom(id) {
+        delete this.rooms[id];
+    }
+
+    appendRoom(room) {
+        if (!room || !(room instanceof RoomIO)) {
+            return;
+        }
+
+        this.rooms[room.id] = room;
+    }
+
+    getRoom(roomID) {
+        return this.rooms[roomID];
     }
 
     static buildNamespace(setup) {
