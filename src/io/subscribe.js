40028b683eef0b4edd98f95d93e9363f6d8a94c3
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-05-21 18:12:18 -0300

    [HAPI-147] Create new option to use custom load methods (#168)
    ## [HAPI-147] Description
    Adding possibility of use custom methods to load docs and query

--- src/io/subscribe.js
+++ src/io/subscribe.js
@@ -1,14 +1,27 @@
-const { SubscriberIO, CRUD } = require('4hands-api')
+const SubscriberIO = require('../services/ServerIO/SubscriberIO');
+const FS = require('../services/FS');
+const path = require('path');
 
 const PATH = '/subscribe-changes';
+const CUSTOM_CONFIGS_PATH = path.normalize(__dirname.replace(path.normalize('/node_modules/4hands-api/src/io'), ('/src/io' + PATH + '.config.js')));
+const IS_CUSTOM_CONFIGS_EXIST = FS.isExist(CUSTOM_CONFIGS_PATH);
+let CUSTOM_CONFIGS = {};
+
+if (IS_CUSTOM_CONFIGS_EXIST) {
+    CUSTOM_CONFIGS = require(CUSTOM_CONFIGS_PATH);
+}
+
 module.exports = SubscriberIO.buildSubscriber({
     path: PATH,
+
     onConnect(socket) {
         console.log(`User is connected on ${PATH}:`, socket.id);
     },
+
     onDisconnect(socketID) {
         console.log(`User is disconnected from ${PATH}:`, socketID);
     },
+
     onSubscribe(socket, setup, callback = () => {}) {
         const { type, collection, filter, docUID, options } = Object(setup);
 
@@ -22,10 +35,12 @@ module.exports = SubscriberIO.buildSubscriber({
             callback(subscription);
         }
     },
+
     onCreate(collection, docSnapshot) {
         const querySubscriptions = this.getQuerySubscriptions(collection);
         querySubscriptions.map(sub => sub.exec('save', docSnapshot));
     },
+
     onUpdate(collection, docSnapshot) {
         const querySubscriptions = this.getQuerySubscriptions(collection);
         if (Array.isArray(querySubscriptions)) {
@@ -37,10 +52,10 @@ module.exports = SubscriberIO.buildSubscriber({
             docSubscriptions.map(sub => sub.exec('update', docSnapshot));
         }
     },
-    onDelete() {
 
-    },
     onError(err) {
         throw logError(err);
-    }
+    },
+
+    ...CUSTOM_CONFIGS
 });
