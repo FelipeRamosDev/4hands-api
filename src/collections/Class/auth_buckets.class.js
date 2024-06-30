c7bf2224f9e2280e4fba5ab7788ff2185971305e
FelipeRamosDev <49094475+FelipeRamosDev@users.noreply.github.com>
2024-06-30 13:28:58 -0300

    [HAPI-187] Adjust sign-up and sign-in for the new format (#205)
    ## [HAPI-187] Description
    Adjusted the user login for the new format

--- src/collections/Class/auth_buckets.class.js
+++ src/collections/Class/auth_buckets.class.js
@@ -1,7 +1,16 @@
 const AuthBucket = require('4hands-api/src/models/collections/AuthBucket');
+const AuthService = require('4hands-api/src/services/Auth');
 
 class AuthBucketsClass {
     static Model = AuthBucket;
+
+    /**
+     * The AuthService instance associated with this AuthBucket.
+     * @property {AuthService}
+     */
+    get service() {
+        return new AuthService({ parent: this });
+    }
 }
 
 module.exports = AuthBucketsClass;
