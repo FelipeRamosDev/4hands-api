1faddc394167254148b17ef00b1f0a86c0335036
FelipeRamosDev <felipe@feliperamos.info>
2024-07-12 15:49:38 -0300

    [HAPI-210] Update the documentation
    [HAPI-210] Description
    Adding docstrings

--- client/index.js
+++ client/index.js
@@ -2,7 +2,21 @@ const DBQuery = require('./models/DBQuery');
 const AJAX = require('./services/AJAX');
 const Utils = require('./utils');
 
+/**
+ * The main instance class to be declared in order to use the 4hands-api on client side.
+ * @class
+ */
 class _4HandsAPIClient {
+   /**
+    * @constructor
+    * @param {Object} setup - The setup params.
+    * @param {AJAX} setup.ajaxConfig - The configuration for the AJAX instance.
+    * @param {string} [setup.apiHost='http://localhost'] - 
+    * @param {number} [setup.apiPort=8000] - The port to access the backend with 4hands-api server.
+    * @param {SocketIO[]} [setup.sockets=[]] - Socket instances to initialized.
+    * @param {boolean} [setup.useSubscription=false] - If you want to use the database docs subscriptions, this needs to be set as true.
+    * @param {number} [setup.subscriptionPort=5000] - The socket connection port on backend that is listen to /subscribe-changes socket route.
+    */
    constructor (setup) {
       const {
          ajaxConfig,
@@ -34,14 +48,29 @@ class _4HandsAPIClient {
       }
    }
 
+   /**
+    * @readonly
+    * @returns {string} - The api url for requests
+    */
    get apiURL() {
       return `${this.apiHost}:${this.apiPort}`;
    }
 
+   /**
+    * Model to interact with database on backend side.
+    * @param {string} collection - The collection name.
+    * @param {Object} filter - The mongoose filter to use.
+    * @returns {DBQuery} - Retrieve the DBQuery model.
+    */
    dbQuery(collection, filter) {
       return new DBQuery(collection, filter, this);
    }
 
+   /**
+    * Creates a new Socket connection
+    * @param {object} setup 
+    * @returns {void}
+    */
    createSocket(setup) {
       const SocketIO = require('./services/SocketIO');
       
@@ -53,10 +82,19 @@ class _4HandsAPIClient {
       }
    }
 
+   /**
+    * Retrieve a Socket instance already existent.
+    * @param {string} path - SocketIO name to get.
+    * @returns {SocketIO} - The SocketIO instance.
+    */
    getSocket(path) {
       return this._sockets.get(path);
    }
 
+   /**
+    * Closes the given SocketIO instance.
+    * @param {string} path - SocketIO name to close.
+    */
    closeSocket(path) {
       this._sockets.delete(path);
    }
