3cfb23f7ac5e4efb48cd876320aabd6f67ae2c4d
FelipeRamosDev <felipe@feliperamos.info>
2024-08-21 00:37:13 -0300

    [HAPI-217] Adding missing docstrings

--- client/models/Auth.js
+++ client/models/Auth.js
@@ -1,18 +1,39 @@
 const config = require('../config.json');
 
-module.exports = class Auth {
+/**
+ * The Auth service with some methods on it to handle authentication and user matters.
+ * @class Auth
+ */
+class Auth {
+   /**
+    * Creates an instance of the Auth class.
+    * @param {Object} instance - The instance of the application or service using this Auth class.
+    */
    constructor (instance) {
       this._instance = () => instance;
    }
 
+   /**
+    * Returns the current instance.
+    * @returns {Object} - The instance of the application or service.
+    */
    get instance() {
       return this._instance();
    }
 
+   /**
+    * Calculates and returns the cookie expiration age.
+    * @returns {number} - The expiration time for the session cookie in milliseconds.
+    */
    get cookieAge() {
       return Date.now() + config.session.cookieAge;
    }
 
+   /**
+    * Checks whether the user is authorized.
+    * @returns {Promise<Object>} - An object indicating whether the user is logged in.
+    * @throws {Error} - If there is an error other than user not being authorized.
+    */
    async checkUser() {
       try {
          return await this.instance.ajax.authGet('/auth/auth-check');
@@ -25,6 +46,13 @@ module.exports = class Auth {
       }
    }
 
+   /**
+    * Logs in the user with the provided email and password.
+    * @param {string} email - The user's email address.
+    * @param {string} password - The user's password.
+    * @returns {Promise<Object>} - An object containing the login result or an error.
+    * @throws {Error} - If email or password is missing, or if another error occurs.
+    */
    async login(email, password) {
       if (!email || !password) {
          throw new Error('The params "email" and "password" are required!');
@@ -43,6 +71,12 @@ module.exports = class Auth {
       }
    }
 
+   /**
+    * Registers a new user with the provided data.
+    * @param {Object} data - The data required for user registration.
+    * @returns {Promise<Object>} - An object containing the registration result.
+    * @throws {Error} - If registration fails.
+    */
    async register(data) {
       try {
          const created = await this.instance.ajax.post('/auth/register', data);
@@ -55,6 +89,11 @@ module.exports = class Auth {
       }
    }
 
+   /**
+    * Signs out the current user.
+    * @returns {Promise<Object>} - An object indicating whether the sign-out was successful.
+    * @throws {Error} - If sign-out fails.
+    */
    async signOut() {
       try {
          return await this.instance.ajax.authPost('/auth/signout');
@@ -67,3 +106,5 @@ module.exports = class Auth {
       }
    }
 }
+
+module.exports = Auth;
