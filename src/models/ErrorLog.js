7c4974a2f7b19f7929a5a5011947a260e114ae84
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2024-12-10 10:21:40 -0300

    [v0.7.7][HAPI-234] Remove FS approach from the errors log (#263)
    * [HAPI-235] Removing the "write file" method (#261)
    Removing the FS approach

--- src/models/ErrorLog.js
+++ src/models/ErrorLog.js
@@ -1,110 +1,87 @@
-const fs = require('fs');
-const path = require('path');
-
 class ErrorLog {
-    constructor (err, merge = {}) {
-        this.error = true;
+   constructor(err, merge = {}) {
+      this.error = true;
 
-        if (!err) {
-            this.name = 'UNKNOWN_ERROR';
-            this.message = 'An unknow error was caught!'
-        }
+      if (!err) {
+         this.name = 'UNKNOWN_ERROR';
+         this.message = 'An unknow error was caught!'
+      }
 
-        if (typeof err === 'string') {
-            const rsc = Resource.error(err);
+      if (typeof err === 'string') {
+         const rsc = Resource.error(err);
 
-            if (rsc) {
-                this.name = rsc.name;
-                this.message = rsc.message;
-            } else {
-                this.message = err;
-            }
-        }
+         if (rsc) {
+            this.name = rsc.name;
+            this.message = rsc.message;
+         } else {
+            this.message = err;
+         }
+      }
 
+      if (typeof err === 'object' && !Array.isArray(err)) {
+         const { code, name, message, msg, type, stack } = Object(err);
 
-        if (typeof err === 'object' && !Array.isArray(err)) {
-            const { code, name, message, msg, type, stack } = Object(err);
+         if (code) this.code = code;
+         if (name) this.name = name;
+         if (message || msg) this.message = message || msg;
+         if (type) this.type = type;
+         if (stack) this.stack = stack;
+      }
 
-            if (code) this.code = code;
-            if (name) this.name = name;
-            if (message || msg) this.message = message || msg;
-            if (type) this.type = type;
-            if (stack) this.stack = stack;
-        }
+      if (!this.stack) {
+         this.stack = new Error().stack
+      }
 
-        if (!this.stack) {
-            this.stack = new Error().stack
-        }
+      if (!Array.isArray(merge) && typeof merge === 'object') {
+         Object.keys(merge).map(key => {
+            this[key] = merge[key];
+         });
+      }
+   }
 
-        if (!Array.isArray(merge) && typeof merge === 'object') {
-            Object.keys(merge).map(key => {
-                this[key] = merge[key];
-            });
-        }
-    }
+   get stackArray() {
+      const array = this.stack.split('\n');
 
-    get stackArray() {
-        const array = this.stack.split('\n');
+      array.splice(0, 1);
+      return array.map(item => item.replace('    at ', ''));
+   }
 
-        array.splice(0, 1);
-        return array.map(item => item.replace('    at ', ''));
-    }
+   stackTemplate() {
+      const date = new Date();
+      const msg = this.message || this.msg;
+      let out = `\n[${date.toLocaleDateString()} - ${date.toLocaleTimeString()}]\n`;
 
-    stackTemplate() {
-        const date = new Date();
-        const msg = this.message || this.msg;
-        let out = `\n[${date.toLocaleDateString()} - ${date.toLocaleTimeString()}]\n`;
+      if (this.name || msg) out += `${this.name || 'Error'}: ${msg ? msg : ''}\n`;
+      if (this.stack) out += 'Stack: ' + this.stackArray.join('\n    ');
+      out += '\n--------------------------------------------------------------------------------------------------------------------------------\n\n';
 
-        if (this.name || msg) out += `${this.name || 'Error'}: ${msg ? msg : ''}\n`;
-        if (this.stack) out += 'Stack: ' + this.stackArray.join('\n    ');
-        out += '\n--------------------------------------------------------------------------------------------------------------------------------\n\n';
+      return out;
+   }
 
-        return out;
-    }
+   toObject() {
+      return { ...this };
+   }
 
-    toObject() {
-        return { ...this };
-    }
+   print() {
+      console.error(this.stackTemplate(), '\n');
+   }
 
-    print() {
-        console.error(this.stackTemplate(), '\n');
-    }
+   printWarn() {
+      console.warn(this.stackTemplate(), '\n');
+   }
 
-    printWarn() {
-        console.warn(this.stackTemplate(), '\n');
-    }
+   append() { }
 
-    async writeLog() {
-        const projectPath = path.normalize(__dirname.replace(path.normalize('/node_modules/4hands-api/src/models'), '/'));
-        const logFilePath = path.join(projectPath, 'errors.log');
-        const logString = this.stackTemplate();
-        
-        // Check if the log file exists
-        if (!fs.existsSync(logFilePath)) {
-          // Create the file if it doesn't exist
-          fs.writeFileSync(logFilePath, '');
-        }
-        
-        // Append the log message to the file
-        fs.appendFile(logFilePath, logString, (err) => {
-          if (err) {
-            console.error('Error writing to the log file:', err);
-          }
-        });
-    }
+   static logError(err) {
+      const error = new ErrorLog(err);
 
-    append() {}
+      console.error(error.stackTemplate());
+      return error;
+   }
 
-    static logError(err) {
-        const error = new ErrorLog(err);
-        
-        error.writeLog();
-        return error;
-    }
-
-    static toError(err) {
-        return new ErrorLog(err);
-    }
+   static toError(err) {
+      return new ErrorLog(err);
+   }
 }
 
 module.exports = ErrorLog;
